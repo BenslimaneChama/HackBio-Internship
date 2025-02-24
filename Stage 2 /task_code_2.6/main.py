0bfdfbefdb1ad31fd0431241fad8d9a8ccb7b97b
Mohammad HICHAM POLO <mohammad.hicham@um5r.ac.ma>
2025-02-24 00:40:15 +0000

    Update main.py

--- Stage 2 /task_code_2.6/main.py
+++ Stage 2 /task_code_2.6/main.py
@@ -11,7 +11,7 @@ Stage 2 GitHub Repository: https://github.com/BenslimaneChama/HackBio-Internship
 Task_code_2.6 Github Repository: https://github.com/BenslimaneChama/HackBio-Internship/tree/main/Stage%202%20/task_code_2.6
 Plots images Github Repository : https://github.com/BenslimaneChama/HackBio-Internship/tree/main/Stage%202%20/task_code_2.6/figures
 
-LinkedIn Video: STILL
+LinkedIn Video: https://www.linkedin.com/feed/update/urn:li:ugcPost:7299588294600781824/
 '''
 #___________________________________________________________________________________________________________________________________________
 # Required Modules
